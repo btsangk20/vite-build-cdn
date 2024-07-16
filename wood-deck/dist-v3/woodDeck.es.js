f3f98c34ea1d14fc5707d2a442514d85f4408396
Bùi Tấn Sang <sang.t.bui@tgl-sol.com>
2024-07-16 11:20:32 +0700

    super big update

---
+++ wood-deck/dist-v3/woodDeck.es.js
@@ -0,0 +1,4 @@
+const t = (o, s) => o + s;
+export {
+  t as sum
+};
---
+++ wood-deck/dist-v3/woodDeck.iife.js
@@ -0,0 +1,1 @@
+var WoodDeck=function(e){"use strict";const t=(u,n)=>u+n;return e.sum=t,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),e}({});
